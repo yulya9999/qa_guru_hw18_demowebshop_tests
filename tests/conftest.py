1a49eea5d2a32809bf8dee47efa5ad64d4fc7ce5
Yulia Vdovina <yulya_vdovina_2000@mail.ru>
2024-07-22 21:16:12 +0300

    hw18

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,17 +1,6 @@
 import pytest
 from selene import browser
 from tests.data.base_func import user_auth, open_browser
-from dotenv import load_dotenv
-import os
-
-
-# @pytest.fixture(scope="session", autouse=True)
-# def load_env():
-#     load_dotenv()
-#
-#
-# login_shop = os.getenv("LOGIN")
-# pass_shop = os.getenv("PASS")
 
 
 @pytest.fixture(scope="session", autouse=True)
